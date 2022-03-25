71e55c5452567c49ceddcb3bf363d87a1c219c12
Don <kibetdonald97@gmail.com>
2022-03-25 14:53:51 +0300

    Added react front end build

--- deployToHeroku/settings.py
+++ deployToHeroku/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'deployToHeroku.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -65,6 +65,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect'
             ],
         },
     },
