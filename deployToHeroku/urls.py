d7976f5f31153ec40fcc29e2b167f8dd46b42762
Don <kibetdonald97@gmail.com>
2022-03-25 14:47:02 +0300

    Added react front end build

--- deployToHeroku/urls.py
+++ deployToHeroku/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+# react path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+# regular expression for handling routes
+urlpatterns += [re_path(r'^.*',
+                        TemplateView.as_view(template_name='index.html'))]
+
