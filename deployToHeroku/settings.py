3fe1ff965ecd9faee15288fbf1f8753baf7f4091
Don <kibetdonald97@gmail.com>
2022-03-25 15:00:31 +0300

    Added react front end build

--- deployToHeroku/settings.py
+++ deployToHeroku/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'deployToHeroku.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'build')],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -65,12 +65,10 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'social_django.context_processors.backends',
-                'social_django.context_processors.login_redirect'
             ],
         },
     },
-]
+] 
 
 WSGI_APPLICATION = 'deployToHeroku.wsgi.application'
 
--- deployToHeroku/urls.py
+++ deployToHeroku/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, re_path
-# react path
-from django.views.generic import TemplateView
+from django.urls import path, include
 
-# regular expression for handling routes
-urlpatterns += [re_path(r'^.*',
-                        TemplateView.as_view(template_name='index.html'))]
+urlpatterns = [
+    path('admin/', admin.site.urls)
+]
 
